aae7f0d16d8fdf904bde0c3279d240f5f98f5776
Alex Telon <alex.telon@gmail.com>
2018-02-25 22:40:57 +0100

    Gave basic stats to our Hero and options around them
    And the options one get every level relate to these now.

--- Hero.cs
+++ Hero.cs
@@ -14,8 +14,16 @@ namespace CustomHeroCreator
         public bool IsActive { get; internal set; } = true;
 
 
-        // tmp test thing, not to be a string later on
-        public string Skills { get; private set; }
+        public string Stats => "Strength: " + Str + " Agility: " + Agi + " Intelligence: " + Int;
+
+        // Some stats for the Hero
+
+        public int Str { get; private set; } = 1;
+        public int Agi { get; private set; } = 1;
+        public int Int { get; private set; } = 1;
+
+
+
 
         /// <summary>
         /// Level up the user (and give the user the option to choose new skills
@@ -37,40 +45,69 @@ namespace CustomHeroCreator
 
             var skillOptions = GenerateRandomSkills();
 
-            var i = 1;
-            foreach (var skill in skillOptions)
+            var hasChoosen = false;
+
+            while (!hasChoosen)
             {
-                Console.WriteLine("[" + i++ + "]: " + skill);
+
+
+                var i = 1;
+                foreach (var skill in skillOptions)
+                {
+                    Console.WriteLine("[" + i++ + "]: " + skill.Key + " +" + skill.Value);
+                }
+
+
+                var input = Console.ReadLine();
+
+                // abort
+                if (input == "q" || input == "Q")
+                {
+                    IsActive = false;
+                    hasChoosen = true;
+                    break;
+                }
+
+                int option = int.Parse(input);
+
+                switch (option)
+                {
+                    case 1:
+                        Str += skillOptions["str"];
+                        hasChoosen = true;
+                        break;
+                    case 2:
+                        Agi += skillOptions["agi"];
+                        hasChoosen = true;
+                        break;
+                    case 3:
+                        Int += skillOptions["int"];
+                        hasChoosen = true;
+                        break;
+
+                    default:
+                        hasChoosen = false;
+                        break;
+                }
+
+                if (!hasChoosen)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Please choose a valid option");
+                }
+                Console.WriteLine();
             }
-
-
-            var input = Console.ReadLine();
-
-            // abort
-            if (input == "q" || input == "Q")
-            {
-                IsActive = false;
-            }
-
-
-            int option = int.Parse(input);
-
-            // option is 1 indexed hence the - 1
-            Skills += skillOptions[option - 1] + " ";
-
-            Console.WriteLine();
         }
 
-        private List<string> GenerateRandomSkills()
+        private Dictionary<string, int> GenerateRandomSkills()
         {
-            var skills = new List<string>();
+            var skills = new Dictionary<string, int>();
 
             Random rnd = new Random();
 
-            for (int i = 0; i < SKILL_OPTIONS_PER_LVL_UP; i++)
-            {
-                skills.Add("Strenght " + rnd.Next(1, 5));
-            }
+            skills.Add("str", rnd.Next(1, 5));
+            skills.Add("agi", rnd.Next(1, 5));
+            skills.Add("int", rnd.Next(1, 5));
 
             return skills;
         }
@@ -84,7 +121,7 @@ namespace CustomHeroCreator
 
         public override string ToString()
         {
-            return "Name: " + Name + " Level: " + Level + " Skills: " + Skills;
+            return "Name: " + Name + " Level: " + Level + " Stats: " + Stats;
         }
 
     }
