daba1a27197fbaa3b1b205b86f3e51981da5cced
Alex Telon <alex.telon@gmail.com>
2018-03-10 17:09:25 +0100

    Added simple unit test project
    Added two simple performance tests.

---
+++ XUnitTests/PerformanceTest.cs
@@ -0,0 +1,93 @@
+using CustomHeroCreator;
+using CustomHeroCreator.AI;
+using CustomHeroCreator.CLI;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using Xunit;
+
+namespace XUnitTests
+{
+    public class PerformanceTest
+    {
+        //[Fact]
+        //public void Test1()
+        //{
+        //    Stopwatch sw = new Stopwatch();
+        //    sw.Start();
+        //    var rnd = new Random();
+
+        //    // Create the arena in which the heroes fitness will be evaluated
+        //    Arena arena = new Arena();
+
+        //    // Trials decide what type of trials the heroes will meet (single player survival mode or gladiator arena like stuff)
+        //    Trials trials = new Trials();
+        //    trials.MaxLevel = 5000;
+
+        //    var evo = new Evolution(rnd, skillTreeGenerator: null);
+        //    evo.NrOfHeroesInEachGeneration = 100;
+        //    evo.MaxGenerations = 20;
+        //    evo.HeroStartingLevel = 10;
+        //    evo.AllwaysRunAllGenerations = true;
+
+        //    evo.RunEvolution(trials, arena);
+
+        //    sw.Stop();
+
+        //    // when this test was made it took 13 and 11 sec just as a benchmark.
+        //    // make sure we are not getting too slow here!
+        //    Assert.False(sw.ElapsedMilliseconds > 20000);
+        //}
+
+        [Fact]
+        public void LevelUpPerformanceTest()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var rnd = new Random();
+
+            var hero = new Hero(rnd);
+            var agent = new Agent(rnd);
+
+            hero.AI = agent;
+
+            for (int i = 0; i < 100000; i++)
+            {
+                hero.LevelUp();
+            }
+
+            sw.Stop();
+
+            Assert.False(sw.ElapsedMilliseconds > 250);
+        }
+
+
+        [Fact]
+        public void FightPerformanceTest()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var rnd = new Random();
+
+            var hero = new Hero(rnd);
+            hero.AI = new Agent(rnd);
+
+            var enemy = new Hero(rnd);
+            enemy.AI = new Agent(rnd);
+
+            var arena = new Arena();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                arena.Fight(hero, enemy);
+            }
+
+            sw.Stop();
+
+            Assert.False(sw.ElapsedMilliseconds > 10);
+        }
+
+
+
+    }
+}
